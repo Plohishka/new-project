c3b40913839a790869d12d7efe751ce090a1976e
Oleksandr <plohishi.family@gmail.com>
2020-05-21 13:28:23 +0300

    new js

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false
-};
-let a1 = ("Введите обязательную статью расходов в этом месяце", ""),
-    a2 = ("Во сколько обойдется?", ""),
-    a3 = ("Введите обязательную статью расходов в этом месяце", ""),
-    a4 = ("Во сколько обойдется?", "");
-    appData.expenses.a1 = a2;
-    appData.expenses.a3 = a4;
-    alert(appData.budget / 30);
+function plus() {
+    var num1, num2, result;
+    num1 = document.getElementById("n1").value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById("n2").value;
+    num2 = parseInt(num2);
+
+    result = num1 + num2;
+    document.getElementById("out").innerHTML = result;
+}
+
+function minus() {
+    num1 = document.getElementById("n1").value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById("n2").value;
+    num2 = parseInt(num2);
+
+    result = num1 - num2;
+    document.getElementById("out").innerHTML = result;
+}
+
+function multiply() {
+    num1 = document.getElementById("n1").value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById("n2").value;
+    num2 = parseInt(num2);
+
+    result = num1 * num2;
+    document.getElementById("out").innerHTML = result;
+}
